0af824f8640a11d38973b420d0344d7ff9ea8645
Matthew Justin Bauer <mjbauer95@gmail.com>
2015-07-12 17:32:55 -0500

    Move testing deps to devDependencies.

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ System.config({
     "fetch": "github:github/fetch@0.9.0",
     "fs": "github:jspm/nodelibs-fs@0.1.2",
     "mocha": "npm:mocha@2.2.5",
-    "traceur": "github:jmcriffey/bower-traceur@0.0.90",
+    "traceur": "github:jmcriffey/bower-traceur@0.0.88",
     "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.88",
     "github:jspm/nodelibs-buffer@0.1.0": {
       "buffer": "npm:buffer@3.2.2"
