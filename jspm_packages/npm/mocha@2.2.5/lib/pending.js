275e6c274bd9b579e64820294a5ec7765a768235
Matthew Justin Bauer <mjbauer95@gmail.com>
2015-07-19 23:20:18 -0500

    Add jspm_packages to gh-pages for testing.

---
+++ jspm_packages/npm/mocha@2.2.5/lib/pending.js
@@ -0,0 +1,18 @@
+/* */ 
+"format cjs";
+
+/**
+ * Expose `Pending`.
+ */
+
+module.exports = Pending;
+
+/**
+ * Initialize a new `Pending` error with the given message.
+ *
+ * @param {String} message
+ */
+
+function Pending(message) {
+    this.message = message;
+}
